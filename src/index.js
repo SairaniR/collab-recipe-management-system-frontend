0d22364b904dd9273e026e1cf45eef3198d0431a
SairaniR <47656881+SairaniR@users.noreply.github.com>
2024-08-30 20:31:42 +0000

    login template

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles.css'; // Import your styles
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './components/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Login />
   </React.StrictMode>
 );
 
